0279b123772c0a244e6054fd7e1b581c5b86ce48
Eric Arnebäck <arnebackeric@gmail.com>
2018-04-23 13:24:07 -0700

    clean up some

--- src/logic.h
+++ src/logic.h
@@ -902,26 +902,17 @@ vec3 pal( in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d )
     return a + b*cos( 6.28318*(c*t+d) );
 }
 
-
 float quarticIn(float t) {
   return pow(t, 4.0);
 }
 
 vec3 colorize(float t, vec2 uv) {
-    float p = 0.2;
-     vec3 col = vec3(0.0, 0.0, 0.0);
- //    t += (-p + 2.0 * p * mynoise(uv * 3000.0  ));
+  float p = 0.2;
+  vec3 col = vec3(0.0, 0.0, 0.0);
 
-     //col = pal( t, vec3(0.2,0.2,0.2),vec3(0.2,0.2,0.2),vec3(0.3,0.2,0.2),vec3(0.2,0.15,0.90) );
+  t += float(uCounter) / 200;
+  col = 1.2 * pal( t, vec3(0.5,0.5,0.5),vec3(0.5,0.5,0.5),vec3(1.0,1.0,1.0),vec3(0.0,0.33,0.67) );
 
-t += float(uCounter) / 200;
-col = 1.2 * pal( t, vec3(0.5,0.5,0.5),vec3(0.5,0.5,0.5),vec3(1.0,1.0,1.0),vec3(0.0,0.33,0.67) );
-
-//col = pal( t, vec3(0.5,0.5,0.5),vec3(0.5,0.5,0.5),vec3(2.0,1.0,0.0),vec3(0.5,0.20,0.25) );
-
- 
-
-    // col *= ( pow(t, 4.0) );
  return col;
 }
 
